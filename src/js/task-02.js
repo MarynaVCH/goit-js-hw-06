a50dcef678b6d6e6428c362297d3c1d88adbc381
MarynaVCH <83900520+MarynaVCH@users.noreply.github.com>
2021-10-10 13:36:02 +0300

    solution task 02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const itemEl = document.querySelector('#ingredients');
+
+ingredients.forEach(ingredient => {
+  const vegetables = document.createElement('li');
+  vegetables.classList.add('item');
+  vegetables.textContent = ingredient;
+
+  itemEl.append(vegetables);
+});
+
+console.log(itemEl);
